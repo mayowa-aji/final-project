13aeb6bf5bfbfa30141f12309224adddab8cf8a6
Mayowa Ajiboye <64916584+mayowa-aji@users.noreply.github.com>
2023-04-07 15:14:38 -0400

    add carousel to homepage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from './Header';
+import Marquee from './Marquee';
 
 const HomePage = () => {
   return (
     <div>
       <Header />
+      <Marquee />
     </div>
   );
 }
