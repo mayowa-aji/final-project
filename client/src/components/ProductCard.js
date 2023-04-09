8d336ea2b649faf2c7c8d058b5c72c1cdda49cbb
Mayowa Ajiboye <64916584+mayowa-aji@users.noreply.github.com>
2023-04-08 22:35:50 -0400

    remove border

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -4,7 +4,7 @@ import { Card} from 'react-bootstrap';
 const ProductCard = ({ imageUrl, name, price }) => {
   // const imagePath = require(`../images/${imageName}`).default;
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ width: '18rem', border:'none' }}>
       <Card.Img variant="top" src={imageUrl} alt={name} />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
